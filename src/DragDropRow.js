e40d839624fcc3ac084c9d32c2a6c7c4ea3aa264
ASUS <Yehor Kholodiuk>
2024-11-11 18:23:20 +0200

    commiting

---
+++ src/DragDropRow.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DragDropRow = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default DragDropRow;
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <h1>
+            Drag and Drop Example
+        </h1>
+    );
+};
+
+export default Header;
